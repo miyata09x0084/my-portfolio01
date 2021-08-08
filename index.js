b1ef4cf67d7e8e30d0aa8b8ac1bb30e0819a7274
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-08-08 17:19:27 +0900

    sceneとcameraを追加する

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 
   import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
 
-  const scene = new THREE.Scene();
-	console.log(scene)
+  let scene, camera;
+
+  function init() {
+    scene = new THREE.scene();
+    camera = new THREE.PerspecstiveCamera(60, window.innerWidth, window.innerHeight, 1, 1000);
+    camera.position.z = 1;
+    camera.rotation.x = 1.16;
+    camera.rotation.y = -0.12;
+    camera.rotation.z = 0.27;
+  }
